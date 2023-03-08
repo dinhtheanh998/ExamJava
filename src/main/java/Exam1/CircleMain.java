66a2f41b0bea32c9184161044fd6e965774015e8
dinhtheanh998 <dinhtheanh198@gmail.com>
2023-03-08 15:35:18 +0700

    Update code and images

--- src/main/java/Exam1/CircleMain.java
+++ src/main/java/Exam1/CircleMain.java
@@ -2,10 +2,14 @@ package Exam1;
 
 public class CircleMain {
     public static void main(String[] args) {
-        Circle c1 = new Circle(3.0);
-        System.out.println(c1);
-        Circle c2 = new Circle(2.0);
-        System.out.println(c2);
+        try {
+            Circle c1 = new Circle(-3.0);
+            System.out.println(c1);
+            Circle c2 = new Circle(2.0);
+            System.out.println(c2);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
